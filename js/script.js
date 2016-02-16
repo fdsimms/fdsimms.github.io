2c7d2293a84a0d078730e8931132150269f9e9e8
Frankie <fdsimms@gmail.com>
2016-02-16 10:46:42 -0500

    Get rid of nicescroll script file.

--- js/script.js
+++ js/script.js
@@ -34,35 +34,17 @@ $('.owl-carousel').owlCarousel({
             items:1
         }
     }
-})
-
-$(document).ready(
-
-  function() { 
-
-    $("html").niceScroll({
-    	cursorcolor:"#f74d65",
-    	scrollspeed :"100",
-    	cursorborder:"1px solid #f74d65",
-    	horizrailenabled: "false",
-    	cursorborderradius: "0px"
-    });
-
-  }
-
-);
+});
 
 new WOW().init();
 
 
-    
+
 /*Preloader*/
 //<![CDATA[
 $(window).load(function() { // makes sure the whole site is loaded
   $('#status').fadeOut(); // will first fade out the loading animation
   $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
   $('body').delay(350).css({'overflow':'visible'});
-})
+});
 //]]>
-
-
